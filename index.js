2676feada2216e731a3e67e75db94849c48afd34
MERCYCHEROP <mercy.cherop@student.moringaschool.com>
2026-04-16 12:16:18 +0300

    weather

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 // index.js
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
-// Your code here!
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const display = document.getElementById('alerts-display');
+const errorDiv = document.getElementById('error-message');
+
+button.addEventListener('click', async () => {
+  const state = input.value.trim();
+
+  try {
+    const res = await fetch(
+      `https://api.weather.gov/alerts/active?area=${state}`
+    );
+
+    const data = await res.json();
+
+    const alerts = data.features;
+
+    // CLEAR UI FIRST
+    display.innerHTML = "";
+    errorDiv.textContent = "";
+    errorDiv.classList.add("hidden");
+
+    // SHOW DATA
+    display.textContent = `${data.title}: ${alerts.length}`;
+
+    alerts.forEach(alert => {
+      const p = document.createElement("p");
+      p.textContent = alert.properties.headline;
+      display.appendChild(p);
+    });
+
+    // CLEAR INPUT
+    input.value = "";
+
+  } catch (error) {
+    errorDiv.textContent = error.message;
+    errorDiv.classList.remove("hidden");
+  }
+});
